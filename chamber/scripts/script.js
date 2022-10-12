ad24f21e4bd182dc13203c94052d229d5bc4ec62
ScratchyPDX <ward.david@live.com>
2022-10-12 14:06:45 -0700

    Add "join us" message

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -23,10 +23,13 @@ const month = [
 ];
 
 const date = new Date();
+
+// set footer dates
 document.getElementById("today-date").textContent = `${weekday[date.getDay()]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
 document.getElementById("year").innerHTML = date.getFullYear();
 document.getElementById("last_modified").innerHTML = `Last modified: ${document.lastModified}`;
 
+// toggle hamburger menu
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -34,3 +37,11 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+// Join us message based on day of the week
+if (weekday[date.getDay()] == "Monday" || weekday[date.getDay()] == "Tuesday") {
+  document.getElementById("join-us").style.display = "block";
+}
+else {
+  document.getElementById("join-us-message").style.display = "none";
+}
