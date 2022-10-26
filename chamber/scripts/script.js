dd7062ba3ef15849c2537be62463413e91ba5c17
ScratchyPDX <ward.david@live.com>
2022-10-25 18:16:51 -0700

    Add lazy loading of images

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -40,8 +40,40 @@ x.onclick = toggleMenu;
 
 // Join us message based on day of the week
 if (weekday[date.getDay()] == "Monday" || weekday[date.getDay()] == "Tuesday") {
-  document.getElementById("join-us").style.display = "block";
+  document.getElementById("join-us-message").style.display = "block";
 }
 else {
   document.getElementById("join-us-message").style.display = "none";
+}
+
+// lazy loading of images
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => { image.removeAttribute("data-src");};
+};
+
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
