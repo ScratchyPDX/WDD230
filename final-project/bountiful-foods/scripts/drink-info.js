ac7cabd4941263f8414a94f33719ee1d33e6a41d
David Ward <ward.david@live.com>
2022-12-04 15:21:27 -0800

    Populate fruit select

---
+++ final-project/bountiful-foods/scripts/drink-info.js
@@ -0,0 +1,22 @@
+const requestURL = 'data/drink-data.json';
+
+fetch(requestURL) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    populateSelectControls(jsonObject, '#fruit1');
+    populateSelectControls(jsonObject, '#fruit2');
+    populateSelectControls(jsonObject, '#fruit3');
+  });
+
+  function populateSelectControls(drinks, controlId) {
+    let select = document.querySelector(controlId)
+    for(let x in drinks) {
+      let option =  document.createElement("option")
+      option.innerHTML = drinks[x].name;
+      option.value = drinks[x].name;
+      select.appendChild(option);
+    }
+    select.selectedIndex = -1;
+  }
