0dbdb99188bfbcd9ad110561a3f53503f4644ba3
David Ward <ward.david@live.com>
2022-12-03 18:48:50 -0800

    Fix drink purchase local storage in doesn't exist

--- final-project/bountiful-foods/scripts/modal-dialog.js
+++ final-project/bountiful-foods/scripts/modal-dialog.js
@@ -1,9 +1,13 @@
 function openForm() {
     let drinks_purchased = parseInt(localStorage.getItem("drinks-purchased"));
     // update drink purchased count on local storage
+    if(isNaN(drinks_purchased)) {
+      drinks_purchased = 0;
+    }
     drinks_purchased = drinks_purchased + 1;
     localStorage.setItem("drinks-purchased", drinks_purchased);
 
+    // open model dialog and display order summary
     document.getElementById("myForm").style.display = "block";
   }
   
