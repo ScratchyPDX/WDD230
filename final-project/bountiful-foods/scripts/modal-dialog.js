113a62fea6cfcae2f593e1a4ee4e63965b6d1a51
ScratchyPDX <ward.david@live.com>
2022-12-05 18:08:00 -0800

    Populate confirmation with nutrition values

--- final-project/bountiful-foods/scripts/modal-dialog.js
+++ final-project/bountiful-foods/scripts/modal-dialog.js
@@ -1,12 +1,6 @@
 function openForm() {
-  let drinks_purchased = parseInt(localStorage.getItem("drinks-purchased"));
-  // update drink purchased count on local storage
-  if(isNaN(drinks_purchased)) {
-    drinks_purchased = 0;
-  }
-  drinks_purchased = drinks_purchased + 1;
-  localStorage.setItem("drinks-purchased", drinks_purchased);
-
+  incrementPurchaseCount();
+  
   // open model dialog and display order summary
   const firstName = document.getElementById("fname").value;
   const email = document.getElementById("email").value;
@@ -16,13 +10,7 @@ function openForm() {
   const fruit3 = document.getElementById("fruit3").value;
   const instructions = document.getElementById("instructions").value;
 
-  document.getElementById("conf-fname").textContent = firstName;
-  document.getElementById("conf-email").textContent = email;
-  document.getElementById("conf-phone").textContent = phone;
-  document.getElementById("conf-fruit1").textContent = fruit1;
-  document.getElementById("conf-fruit2").textContent = fruit2;
-  document.getElementById("conf-fruit3").textContent = fruit3;
-  document.getElementById("conf-instructions").textContent = instructions;
+  populateConfirmationInfo(firstName, email, phone, fruit1, fruit2, fruit3, instructions);
   calculateNutritionInfo(fruit1, fruit2, fruit3);
 
   document.getElementById("myForm").style.display = "block";
@@ -31,23 +19,56 @@ function openForm() {
 }
 
 function closeForm() {
-  location.reload();;
+  location.reload();
   document.getElementById("myForm").style.display = "none";
 }
 
-function calculateNutritionInfo(fruit1, fruit2, fruit3) {
-const requestURL = 'data/drink-data.json';
-
-fetch(requestURL) 
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    let fruit1_data = jsonObject.filter(fruit => fruit.name == fruit1);
-    let fruit2_data = jsonObject.filter(fruit => fruit.name == fruit2);
-    let fruit3_data = jsonObject.filter(fruit => fruit.name == fruit3);
-    console.log(`fruit1_data: ${JSON.stringify(fruit1_data)}`)
-    console.log(`fruit2_data: ${JSON.stringify(fruit2_data)}`)
-    console.log(`fruit3_data: ${JSON.stringify(fruit3_data)}`)
-  });
+function incrementPurchaseCount() {
+  let drinks_purchased = parseInt(localStorage.getItem("drinks-purchased"));
+  // update drink purchased count on local storage
+  if(isNaN(drinks_purchased)) {
+    drinks_purchased = 0;
+  }
+  drinks_purchased = drinks_purchased + 1;
+  localStorage.setItem("drinks-purchased", drinks_purchased);
 }
+
+function populateConfirmationInfo(firstName, email, phone, fruit1, fruit2, fruit3, instructions) {
+  document.getElementById("conf-fname").textContent = firstName;
+  document.getElementById("conf-email").textContent = email;
+  document.getElementById("conf-phone").textContent = phone;
+  document.getElementById("conf-fruit1").textContent = fruit1;
+  document.getElementById("conf-fruit2").textContent = fruit2;
+  document.getElementById("conf-fruit3").textContent = fruit3;
+  document.getElementById("conf-instructions").textContent = instructions;
+}
+
+function calculateNutritionInfo(fruit1, fruit2, fruit3) {
+  const requestURL = 'data/drink-data.json';
+  
+  fetch(requestURL) 
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      let fruit1_data = jsonObject.filter(fruit => fruit.name == fruit1)[0];
+      let fruit2_data = jsonObject.filter(fruit => fruit.name == fruit2)[0];
+      let fruit3_data = jsonObject.filter(fruit => fruit.name == fruit3)[0];
+      console.log(`fruit1_data: ${JSON.stringify(fruit1_data)}`)
+      console.log(`fruit2_data: ${JSON.stringify(fruit2_data)}`)
+      console.log(`fruit3_data: ${JSON.stringify(fruit3_data)}`)
+
+      const carbs = fruit1_data.nutritions.carbohydrates + fruit2_data.nutritions.carbohydrates + fruit3_data.nutritions.carbohydrates;
+      const protein = fruit1_data.nutritions.protein + fruit2_data.nutritions.protein + fruit3_data.nutritions.protein;
+      const sugar = fruit1_data.nutritions.sugar + fruit2_data.nutritions.sugar + fruit3_data.nutritions.sugar;
+      const calories = fruit1_data.nutritions.calories + fruit2_data.nutritions.calories + fruit3_data.nutritions.calories;
+      const fat = fruit1_data.nutritions.fat + fruit2_data.nutritions.fat + fruit3_data.nutritions.fat;
+      document.getElementById("carbs").textContent = carbs;
+      document.getElementById("protien").textContent = protein;
+      document.getElementById("sugar").textContent = sugar;
+      document.getElementById("calories").textContent = calories;
+      document.getElementById("fat").textContent = fat;
+  
+    });
+  }
+  
